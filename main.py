84e3e3d9ad0acb812cf4a6e5150b766c5e15e991
Jurupoc <jurupoc222@gmail.com>
2024-01-21 11:55:11 -0300

    Adding templates HTML

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def welcome():
-    return {"Hello": "World"}
+def welcome(request: Request):
+    return templates.TemplateResponse(name="welcome.html", request=request)
